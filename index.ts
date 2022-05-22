9603131990d314fbb49490cc5ca39dc93de3626e
PatrykKeska <PatrykKeska4@gmail.com>
2022-05-22 18:14:50 +0200

    rate-limiter added to project, and set for 100 request per 5min

--- index.ts
+++ index.ts
@@ -4,12 +4,19 @@ import 'express-async-errors';
 import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
+import rateLimit from 'express-rate-limit'
 
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})
+
+app.use(limiter)
 app.use(cors({
     origin: "http://localhost:3000"
 }));
 app.use(json());
-app.get('/',async (req, res) => {
+app.get('/', async (req, res) => {
     throw new ValidationError("No way bro!");
 })
 app.use(handleError);
