6bbea093273bbdafd12af8b6ca91650981158a30
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-05 16:46:08 +0200

    config file to secure a data has added

---
+++ config/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    dbHost: 'localhost',
+    dbUser: 'root',
+    dbPassword: 'root',
+    dbDatabase: 'notice-board',
+    dbPort: 8889,
+
+};
--- utils/db.ts
+++ utils/db.ts
@@ -1,12 +1,12 @@
 import {createPool} from "mysql2/promise";
-
+import {config} from '../config/config';
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'notice-board',
-    port: 8889,
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbDatabase,
+    port: config.dbPort,
     namedPlaceholders: true,
     decimalNumbers: true,
 })
